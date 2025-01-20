489201276f9afdb1d7bb32ba10e5de321b1037ef
Михаил Леднев <@step.krg>
2025-01-20 20:40:32 +0500

    first

---
+++ app.js
@@ -0,0 +1,69 @@
+// let numbers = [1,2,3,4,5,6,7,8]
+// let chet = 0
+// let nechet = 0 
+// for(i of numbers){
+//     if(i%2 == 0){
+//         chet+=1 
+//     }
+//     else{
+//         nechet+=1
+//     }
+// }
+
+// alert(`chet:${chet}  nechet:$z{nechet}`)
+
+
+
+
+
+
+
+// let slovo = document.querySelector('#slovo')
+// let bth = document.querySelector('#bth')
+// let num = document.querySelector('#num')
+// let num2 = document.querySelector('#num2')
+// let znak = document.querySelector('#znak')
+
+// function get_name(){
+//     let number = parseInt(num.value)
+//     let number2 = parseInt(num2.value)
+//     let val = znak.value
+//     if(val == '+'){
+//         alert(number+number2)
+//     }else if(val == '*'){
+//         alert(number*number2)
+//     }else if(val == "/"){
+//         alert(number/number2)
+//     }else if(val == '-'){
+//         alert(number-number2)
+//     }else{
+//         alert('Не поняв')
+//     }
+//     slovo.textContent = 'Джигурда лох'
+// }
+
+// bth.addEventListener('click', get_name)
+
+
+
+let fruit = ['apple', 'orange', 'banana']
+let i = 0
+while (i <= fruit.length){
+    console.log(fruit[i])
+    i++
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
